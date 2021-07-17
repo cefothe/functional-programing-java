eb367e49df7fd28c9f8f9a1a99c08f4e4f27ab22
stefanangelovMM <stefanangelov@mentormate.com>
2021-07-18 00:29:19 +0300

    Function composition

---
+++ src/main/java/com/stefanangelov/composition/Function.java
@@ -0,0 +1,10 @@
+package com.stefanangelov.composition;
+
+@FunctionalInterface
+public interface Function<T,R> {
+	R apply(T t);
+
+	default<V> Function<V, R> compose(Function<V, T> before){
+		return (V v)-> apply(before.apply(v));
+	}
+}
---
+++ src/main/java/com/stefanangelov/composition/FunctionComposition.java
@@ -0,0 +1,12 @@
+package com.stefanangelov.composition;
+
+public class FunctionComposition {
+	public static void main(String[] args) {
+		Function<Square, Integer> fun1 = Square::getArea;
+		Function<Integer, Double> fun2 = Math::sqrt;
+		Function<Square, Double> getSide = fun2.compose(fun1);
+		Square square = new Square();
+		square.setArea(100);
+		System.out.println(getSide.apply(square));
+	}
+}
