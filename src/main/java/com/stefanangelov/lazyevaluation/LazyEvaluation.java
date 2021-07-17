01e77e154b48326853b2b37e20f3cf231e0f5b96
stefanangelovMM <stefanangelov@mentormate.com>
2021-07-18 01:06:02 +0300

    Lazy Evaluation

---
+++ src/main/java/com/stefanangelov/lazyevaluation/LazyEvaluation.java
@@ -0,0 +1,21 @@
+package com.stefanangelov.lazyevaluation;
+
+public class LazyEvaluation {
+	public static void main(String[] args) {
+		Runnable person = () -> new Person("Ivan", 34);
+
+		person.run();
+
+	}
+}
+
+class Person{
+	private String name;
+	private int age;
+
+	public Person(String name, int age) {
+		System.out.println("Initialization");
+		this.name = name;
+		this.age = age;
+	}
+}
