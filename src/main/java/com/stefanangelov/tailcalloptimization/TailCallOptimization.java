3ee5cab36bee6fc279af3295538964b86db17e3a
stefanangelovMM <stefanangelov@mentormate.com>
2021-07-18 01:13:46 +0300

    Tail Recursion

---
+++ src/main/java/com/stefanangelov/tailcalloptimization/TailCallOptimization.java
@@ -0,0 +1,21 @@
+package com.stefanangelov.tailcalloptimization;
+
+public class TailCallOptimization {
+	public static void main(String[] args) {
+
+	}
+
+	public static long reFact(int n){
+		if(n <=1) {
+			return 1;
+		}
+		return n * reFact(n - 1);
+	}
+
+	public static long tailReFact(int n, int a){
+		if(n<=1) {
+			return a;
+		}
+		return tailReFact(n-1, n*a);
+	}
+}
